baf55bf5dc80bfadfd4cf53b3a7562d79e98cb1f
Christian Weichel <chris@gitpod.io>
2021-11-05 13:38:01 +0000

    Add fetch layer

--- commands/fetch.go
+++ commands/fetch.go
@@ -7,10 +7,12 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
+	"strconv"
 
 	"github.com/containerd/containerd/remotes"
 	"github.com/opencontainers/go-digest"
 	ociv1 "github.com/opencontainers/image-spec/specs-go/v1"
+	"github.com/schollz/progressbar/v3"
 	"github.com/urfave/cli/v2"
 )
 
@@ -48,6 +50,35 @@ var Fetch = &cli.Command{
 			Usage:     "fetches data directly without trying to interpret it",
 			ArgsUsage: "<ref>",
 			Action:    actionFetchDirect,
+		},
+		{
+			Name:      "layer",
+			Usage:     "downloads a layer blob",
+			ArgsUsage: "<ref>",
+			Flags: []cli.Flag{
+				&cli.StringFlag{
+					Name:     "position",
+					Aliases:  []string{"p"},
+					Usage:    `Either "first", "last", a positive number indexing layers starting from the first or a negative number indexing layers starting from the last`,
+					Required: true,
+				},
+				&cli.StringFlag{
+					Name:        "output",
+					Aliases:     []string{"o"},
+					TakesFile:   true,
+					DefaultText: "<layer-hash>.tar.gz",
+					Usage:       "output path",
+				},
+				&cli.StringFlag{
+					Name:  "platform",
+					Usage: "select a particular platform",
+				},
+				&cli.BoolFlag{
+					Name:    "quiet",
+					Aliases: []string{"q"},
+				},
+			},
+			Action: actionFetchLayer,
 		},
 	},
 	Flags: []cli.Flag{
@@ -328,6 +359,102 @@ func actionFetchDirect(c *cli.Context) error {
 	defer fres.Close()
 
 	_, err = io.Copy(os.Stdout, fres)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func actionFetchLayer(c *cli.Context) error {
+	pos := c.String("position")
+
+	ref := fromArgsGetRef(c, "manifest")
+
+	dgst, err := fromFlagsGetDigest(c)
+	if err != nil {
+		return err
+	}
+
+	res, err := fromFlagsGetResolver(c)
+	if err != nil {
+		return err
+	}
+
+	ctx, cancel := fromFlagsGetContext(c)
+	defer cancel()
+
+	plt := c.String("platform")
+
+	_, mf, err := interactiveFetchManifestOrIndex(ctx, res, ref, plt, dgst)
+	if err != nil {
+		return err
+	}
+	if len(mf.Layers) == 0 {
+		return fmt.Errorf("manifest has no layer")
+	}
+
+	var (
+		layer ociv1.Descriptor
+		lc    = len(mf.Layers)
+	)
+	switch pos {
+	case "last":
+		layer = mf.Layers[lc-1]
+	case "first":
+		layer = mf.Layers[0]
+	default:
+		pos, err := strconv.Atoi(pos)
+		if err != nil {
+			return err
+		}
+		posAbs := pos
+		if posAbs < 0 {
+			posAbs *= -1
+		}
+		if posAbs > lc {
+			return fmt.Errorf("position exceeds number of layers: %d > %d", posAbs, lc)
+		}
+
+		if pos < 0 {
+			layer = mf.Layers[lc-(posAbs+1)]
+		} else {
+			layer = mf.Layers[posAbs]
+		}
+	}
+
+	fetcher, err := res.Fetcher(ctx, ref)
+	if err != nil {
+		return err
+	}
+
+	dl, err := fetcher.Fetch(ctx, layer)
+	if err != nil {
+		return err
+	}
+	defer dl.Close()
+
+	// TODO: determine file ending based on content type
+	fn := c.String("output")
+	if fn == "" {
+		fn = layer.Digest.Hex() + "tar.gz"
+	}
+	f, err := os.OpenFile(fn, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	var bar io.Writer
+	if c.Bool("quiet") {
+		bar = io.Discard
+	} else {
+		bar = progressbar.DefaultBytes(
+			layer.Size,
+			"downloading "+layer.Digest.String()+" ",
+		)
+	}
+	_, err = io.Copy(io.MultiWriter(f, bar), dl)
 	if err != nil {
 		return err
 	}
--- go.mod
+++ go.mod
@@ -3,11 +3,21 @@ module github.com/csweichel/oci-tool
 go 1.16
 
 require (
-	github.com/containerd/containerd v1.5.5
-	github.com/docker/cli v20.10.7+incompatible
+	github.com/containerd/containerd v1.5.7
+	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
+	github.com/docker/cli v20.10.10+incompatible
 	github.com/docker/distribution v2.7.1+incompatible
 	github.com/docker/docker-credential-helpers v0.6.4 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/opencontainers/go-digest v1.0.0
 	github.com/opencontainers/image-spec v1.0.1
+	github.com/schollz/progressbar/v3 v3.8.3
 	github.com/urfave/cli/v2 v2.3.0
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/net v0.0.0-20211104170005-ce137452f963 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
+	google.golang.org/genproto v0.0.0-20211104193956-4c6863e31247 // indirect
+	google.golang.org/grpc v1.42.0 // indirect
 )
